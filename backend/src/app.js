1b6e7eb884163c784249895a8fafee581e1313b3
adityakumar68455-adi <adityakumar68455@gmail.com>
2026-07-02 18:46:28 +0530

    my first commit

---
+++ backend/src/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const connectDb = require("./config/db.js")
+
+// routes importing
+const authRoutes = require("./routes/authRoutes.js")
+
+// creating app instance
+const app = express()
+
+// configuration of dotenv
+dotenv.config()
+
+// using cors
+app.use(cors)
+app.use(express.json());
+
+// connecting database 
+connectDb()
+
+// routes 
+app.use("api/auth", authRoutes)
+
+app.get("/", (req, res)=>{
+    res.json({
+        "message": "HEy connected successfully"
+    })
+})
+
+module.exports = app;
---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,98 @@
+const User = require("../models/UserSchema.js")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+exports.register = async (req, res) => {
+    try {
+        let {email, password, role} = req.body;
+
+        if (!email || !password || !role){
+            return res.status(400).json({message: "All details must be present", success:false})
+        }
+
+        email = email.trim().toLowerCase();
+
+        if (!EMAIL_REGEX.test(email)) {
+            return res.status(400).json({ 
+            success: false, 
+            message: 'Please enter a valid email address.' 
+            });
+        }
+
+        const existingUser = await User.findOne({email})
+        if (existingUser){
+            return res.status(400).json({message: "User is already present in the database", success:false})
+        }
+
+        if (password.length < 8){
+            return res.status(400).json({success:false ,message: "Min password length should be 8"})
+        }
+
+        // hashed the password
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+        // user created in the databse 
+        const user = await User.create({
+            email,
+            password: hashedPassword,
+            role,
+        })
+
+        const userResponse = { _id: user._id, email: user.email, role: user.role, };
+
+        res.status(201)
+            .json({
+                success: true,
+                message: "User created successfully",
+                userResponse: userResponse
+            })
+    
+    } catch (error) {
+        console.log("Register error", error.message)
+        res.status(500).json({message: error.message})
+    }
+}
+
+exports.login = async (req,res)=> {
+    try {
+        let {email, password} = req.body;
+
+        if (!email || !password){
+            res.status(400).json({message: "Both details mandatory"})
+        }
+
+        email = email.trim().toLowerCase();
+        // check user
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        // compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        // generating jwt token
+        const token = jwt.sign(
+            {id: user._id , role: user.role},
+            process.env.JWT_SECRET,
+            {expiresIn: "7d"}
+        );
+
+        const userResponse = { _id: user._id, email: user.email, role: user.role, };
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: userResponse
+        });
+        
+    } catch (error) {
+        console.error("error while login", error.message)
+        res.status(500).json({message: error.message})
+    }
+}
